2695d1b41db78b061c3bb2a7ee730079005f50b4
Alok Kumar Pandey <alokumarpandey76@gmail.com>
2026-09-22 16:22:09 +0530

    feat: add 12_static_methods.py

---
+++ python_Learning's/08_object_oriented_programming/12_static_methods.py
@@ -0,0 +1,8 @@
+class ChaiUtils:
+  @staticmethod
+  def clean_ingredients(text):
+    return [item.strip() for item in text.split(",")]
+
+raw = "Alok   ,Ayush    , kishan "
+cleaned = ChaiUtils.clean_ingredients(raw);
+print(cleaned);
